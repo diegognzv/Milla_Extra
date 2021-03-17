a168e03ad776c2305962e37c060fa8173faa86b5
estef1207 <estefany.16.santamaria@gmail.com>
2021-03-17 11:48:04 +0100

    empezamos con js, se ponen ID en algunos elementos y se inició el codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,16 @@ function description(){
     document.getElementById("plato").style.margin="-35% 0px 0px 0px";
     document.getElementById("plantapic").style.width="230px";
     document.getElementById("plantapic").style.height="230px";
-    document.getElementById("boton").style.margin="-278px 0px 0px 56px";
+    document.getElementById("boton").style.margin="0px 0px 0px 53px";
     document.getElementById("boton").style.justifyContent="space-between";
     document.getElementById("boton").style.width="184px";
     document.getElementById("boton").style.display="flex";
+    document.getElementById("puntuacion").style.margin="0px 0px 0px 34%";
+    document.getElementById("info-box").style.margin="0px 0px 0px 7%";
+    document.getElementById("info-box").style.textAlign="justify";
+    document.getElementById("plantasAromaticas").style.marginTop="90px";
+    document.getElementById("plantasAromaticas").style.color="#7E9357";
+    document.getElementById("plantasAromaticas").style.fontSize="14px";
 
 
 
