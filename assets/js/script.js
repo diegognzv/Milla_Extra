7ba84523e6ca6e8ddbb2aadbde4f6d484284a110
estef1207 <estefany.16.santamaria@gmail.com>
2021-03-17 11:19:16 +0100

    empezamos con js, se ponen ID en algunos elementos y se inició el codigo

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+function description(){
+
+    document.getElementById("base").style.margin="-100% 0px 0px 0px";
+    document.getElementById("header-logo").style.margin="-100% 0px 0px 0px";
+    document.getElementById("slide").style.margin="-100% 0px 0px 0px";
+    document.getElementById("plato").style.margin="-35% 0px 0px 0px";
+    document.getElementById("plantapic").style.width="230px";
+    document.getElementById("plantapic").style.height="230px";
+    document.getElementById("boton").style.margin="-278px 0px 0px 56px";
+    document.getElementById("boton").style.justifyContent="space-between";
+    document.getElementById("boton").style.width="184px";
+    document.getElementById("boton").style.display="flex";
+
+
+
+
+
+
+
+
+
+}
