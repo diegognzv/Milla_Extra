79dee082448cd076dda4edda48d33f2124492cff
estef1207 <estefany.16.santamaria@gmail.com>
2021-03-19 09:52:10 +0100

    adecuacion de seguinda parte, pendiente puntuacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 function description(){
 
-    document.getElementById("base").style.margin="-90% 0px 0px 0px";
+    document.getElementById("base").style.margin="-100% 0px 0px 0px";
     document.getElementById("header-logo").style.margin="-100% 0px 0px 0px";
     document.getElementById("slide").style.margin="-100% 0px 0px 0px";
     document.getElementById("plato").style.width="250px";
@@ -13,15 +13,18 @@ function description(){
     document.getElementById("plantapic").style.width="230px";
     document.getElementById("plantapic").style.height="230px";
     document.getElementById("plantapic").style.marginTop="-10px";
-    document.getElementById("boton").style.margin="24px 0px 0px 53px";
+    document.getElementById("boton").style.margin="24px 0px 0px 50px";
     document.getElementById("boton").style.justifyContent="space-between";
     document.getElementById("boton").style.width="184px";
     document.getElementById("boton").style.display="flex";
+    document.getElementById("boton").style.right="initial";
+    document.getElementById("boton").style.translateX="50%"
     document.getElementById("puntuacion").style.margin="41% 0px 0px -56%";
     document.getElementById("puntuacion").style.width="48px";
     document.getElementById("puntuacion").style.height="48px";
     document.getElementById("info-box").style.margin="0px 0px 0px 7%";
     document.getElementById("info-box").style.textAlign="justify";
+    document.getElementById("info-box").style.height="0";
     document.getElementById("plantasAromaticas").style.marginTop="36%";
     document.getElementById("plantasAromaticas").style.color="#7E9357";
     document.getElementById("plantasAromaticas").style.fontSize="14px";
@@ -30,11 +33,12 @@ function description(){
     document.getElementById("price").style.display="flex";
     document.getElementById("price").style.justifyContent="flex-end";
     document.getElementById("price").style.marginTop="-34px";
-    document.getElementById("pbox").style.width="70%";
+    document.getElementById("pbox").style.width="75%";
     document.getElementById("pbox").style.textAlign= "start";
-    document.getElementById("contenedor-boton").style.display= "none";
+    document.getElementById("contenedor-boton").style.height= "26%";
     document.getElementById("fotos").style.display= "inline-flex";
     document.getElementById("fotos").style.width= "78%";
     document.getElementById("fotos").style.justifyContent= "end";
+    
     
 }
